1975300d77dd96ebb2e18a73988c703342b24a30
zombielifestyle <zombielifestyle@gmail.com>
2013-05-12 15:23:21 +0200

    init

---
+++ Client.php
@@ -0,0 +1,197 @@
+<?php
+
+class sider_Client {
+
+    function __construct($options = array()) {
+        $options = $this->options = array_merge(array(
+            'protocol' => 'tcp',
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'timeout' => 5,
+            #'retries' => 5,
+            'writeBuffer' => 8*1024,
+            'readBuffer' => 8*1024,
+        ), $options);
+        $this->ip = null;
+        $this->pipe = false;
+        $this->pipedCommands = 0;
+        $this->metrics = array(
+            'socketReads' => 0,
+            'socketWrites' => 0,
+            'commandsSend' => 0,
+            'repliesReceived' => 0,
+        );
+    }
+
+    function connect() {
+        if (false === getprotobyname($this->options['protocol'])) {
+            throw new Exception("protocol(".$this->options['protocol'].") not supported");
+        }
+        if (is_null($this->ip)) {
+            $this->ip = gethostbyname($this->options['host']);
+        }
+        if (false === ip2long($this->ip)) {
+            throw new Exception("cannot resolve host(".$this->options['host'].")");
+        }
+        $errNo = $errStr = null;
+        $addr = $this->options['protocol'].'://'.$this->ip.':'.$this->options['port'];
+        $socket = stream_socket_client($addr, $errNo, $errStr, $this->options['timeout']);
+        if (!$socket || $errNo) {
+            throw new Exception("connection error($errNo:$errStr)");
+        }
+        stream_set_timeout($socket, $this->options['timeout']);
+        stream_set_read_buffer($socket, $this->options['readBuffer']);
+        stream_set_write_buffer($socket, $this->options['writeBuffer']);
+        $this->socket = $socket;
+        return true;
+    }
+
+    function disconnect() {
+        if (is_resource($this->socket)) {
+            return fclose($this->socket);
+        }
+        return false;
+    }
+
+    function pipe() {
+        $this->pipe = true;
+        $this->pipedCommands = 0;
+        return $this;
+    }
+
+    function unpipe() {
+        $this->pipe = false;
+        return $this->receive();
+    }
+
+    function metrics() {
+        return $this->metrics;
+    }
+
+    function __call($method, $args) {
+        $method = strtolower($method);
+        $numArgs = count($args);
+        if ($numArgs === 0) {
+            $this->send("$method\r\n");
+            $this->pipedCommands++;
+            $this->metrics['commandsSend']++;
+        } else {
+            if ("mget" == $method) {
+                $args = $args[0];
+            } else if ("hmget" == $method) {
+                $key = array_shift($args);
+                $args = $args[0];
+                array_unshift($args, $key);
+            }
+            array_unshift($args, $method);
+            $this->sendCommands(array($args));
+        }
+        if ($this->pipe) {
+            return $this;
+        }
+        return $this->receive();
+    }
+
+    private function sendCommands($commands) {
+        $s = '';
+        $numArgs = 0;
+        foreach ($commands as $command) {
+            $this->pipedCommands++;
+            $this->metrics['commandsSend']++;
+            foreach ($command as $arg) {
+                if (is_array($arg)) {
+                    foreach ($arg as $k => $v) {
+                        $s.= '$'.strlen($k)."\r\n".$k."\r\n";
+                        $s.= '$'.strlen($v)."\r\n".$v."\r\n";
+                        $numArgs += 2;
+                    }
+                } else {
+                    $s.= '$'.strlen($arg)."\r\n".$arg."\r\n";
+                    $numArgs += 1;
+                }
+            }
+        }
+        $this->send("*$numArgs\r\n$s");
+    }
+
+    private function send($msg) {
+        $this->metrics['socketWrites']++;
+        $len = strlen($msg);
+        $written = fwrite($this->socket, $msg, $len);
+        if (false === $written || $written != $len) {
+            throw new Exception("send error");
+        }
+        return $written;
+    }
+
+    private function receive() {
+        $replies = array();
+        while ($this->pipedCommands > 0) {
+            $this->metrics['socketReads']++;
+            $response = fread($this->socket, 8192);
+            if (empty($response)) {
+                throw new Exception("received empty response. protocol error or timeout");
+            }
+            $replies = array_merge($replies, $this->parse($response));
+        }
+        if (count($replies) === 1) {
+            return reset($replies);
+        }
+        return $replies;
+    }
+
+    private function parse($response) {
+        $type = $response{0};
+        $replies = array();
+        $multiBulkReplies = 1;
+        $end = $start = 0;
+        while ($start < strlen($response)) {
+            $mbulk = false;
+            if ('*' === $type) {
+                $start++;
+                $end = strpos($response, "\r\n");
+                $multiBulkReplies = substr($response, $start, $end - $start);
+                $start = $end + 2;
+                $mbulk = true;
+                $this->pipedCommands--;
+                $this->metrics['repliesReceived']++;
+            }
+            for ($i = 1; $i <= $multiBulkReplies; $i++) {
+                if (!$mbulk) {
+                    $this->pipedCommands--;
+                    $this->metrics['repliesReceived']++;
+                }
+                $type = $response{$start};
+                $start++;
+                $end = strpos($response, "\r\n", $start) + 2;
+                $msg = substr($response, $start, $end - $start - 2);
+                if ('+' === $type) {
+                    if ($msg == 'OK') {
+                        $msg = true;
+                    }
+                    $start = $end;
+                    $replies[] = $msg;
+                } elseif ('-' === $type) {
+                    throw new Exception($msg);
+                } elseif (':' === $type) {
+                    $start = $end;
+                    $replies[] = intval($msg);
+                } elseif ('$' === $type) {
+                    $len = intval($msg);
+                    $start = $end;
+                    if ($len >= 0) {
+                        $replies[] = substr($response, $start, $len);
+                        $start += $len + 2;
+                    } else {
+                        $replies[] = null;
+                        $start += 4;
+                    }
+                } else {
+                    throw new Exception("protocol or parse error");
+                }
+            }
+        }
+        return $replies;
+    }
+
+}
---
+++ ClientTest.php
@@ -0,0 +1,232 @@
+<?php
+
+require __DIR__.'/Client.php';
+
+class SiderClientTest extends PHPUnit_Framework_TestCase {
+
+    function testConnectProtoFailure() {
+        $client = new sider_Client(array('protocol' => 'wurst'));
+        try {
+            $client->connect();
+        } catch (Exception $e) {
+            
+        }
+        if (!$e)
+            $this->fail("expected exception not thrown");
+        if ($e->getMessage() != "protocol(wurst) not supported") {
+            throw $e;
+        }
+    }
+
+    function testConnectHostFailure() {
+        $client = new sider_Client(array('host' => 'veryverywrong'));
+        try {
+            $client->connect();
+        } catch (Exception $e) {
+            
+        }
+        if (!$e)
+            $this->fail("expected exception not thrown");
+        if ($e->getMessage() != "cannot resolve host(veryverywrong)") {
+            throw $e;
+        }
+    }
+
+    function testConnectPortFailure() {
+        $this->markTestIncomplete('its tricky');
+    }
+
+    function testConnectAndDisconnect() {
+        $client = new sider_Client();
+        $connected = $client->connect();
+        $this->assertTrue($connected);
+        $disconnected = $client->disconnect();
+        $this->assertTrue($disconnected);
+    }
+
+    function getClient() {
+        static $client;
+        if (!$client) {
+            $client = new sider_Client();
+            $client->connect();
+        }
+        $client->select(3);
+        $client->flushdb();
+        return $client;
+    }
+    
+    function testPing() {
+        $client = $this->getClient();
+        $this->assertEquals("PONG", $client->ping());
+    }
+
+    function testNonsense() {
+        $client = $this->getClient();
+        try {
+            $client->narwalbacon();
+        } catch (Exception $e) {
+            
+        }
+        if (!$e)
+            $this->fail("expected exception not thrown");
+        if ($e->getMessage() != "ERR unknown command 'narwalbacon'")
+            throw $e;
+    }
+    
+    function testSelect() {
+        $client = $this->getClient();
+        $ok = $client->select(3);
+        $this->assertTrue($ok);
+    }
+
+    function testFlushDb() {
+        $client = $this->getClient();
+        $ok = $client->flushdb();
+        $this->assertTrue($ok);   
+    }
+
+    function testFlushAll() {
+        $client = $this->getClient();
+        $ok = $client->flushall();
+        $this->assertTrue($ok);   
+    }
+
+    function testDbSize() {
+        $client = $this->getClient();
+        $size = $client->dbsize();
+        $this->assertEquals(0, $size);
+    }
+    
+    function testInfo() {
+        $client = $this->getClient();
+        $this->assertTrue(strlen($client->info()) > 500);   
+    }
+
+    function testStrings() {
+        $client = $this->getClient();
+        $ok = $client->set("str", "val");
+        $this->assertTrue($ok);
+        $value = $client->get("str");
+        $this->assertEquals("val", $value);
+        $len = $client->append("str", "val");
+        $this->assertEquals(6, $len);
+        $value = $client->get("str");
+        $this->assertEquals("valval", $value);
+        $ok = $client->setnx("str", "newval");
+        $this->assertEquals(0, $ok);
+        $len = $client->strlen("str");
+        $this->assertEquals(6, $len);
+    }
+
+    function testStringsMSetMGet() {
+        $client = $this->getClient();
+        $data = array(
+            'str1' => 'v1',
+            'str2' => 'v2',
+        );
+        $ok = $client->mset($data);
+        $this->assertTrue($ok);
+        $keys = array_keys($data);
+        $keys[] = 'bacon';
+        $storedData = $client->mget($keys);
+        $expectedData = array_values($data);
+        $expectedData[] = null;
+        $this->assertEquals($expectedData, $storedData);
+    }
+
+    function testStringsCounter() {
+        $client = $this->getClient();
+        $count = $client->incr("narwals");
+        $this->assertEquals(1, $count);   
+        $count = $client->incrby("narwals", 10);
+        $this->assertEquals(11, $count);   
+        $count = $client->decr("narwals");
+        $this->assertEquals(10, $count);   
+        $count = $client->decrby("narwals", 20);
+        $this->assertEquals(-10, $count);   
+    }
+
+    function testStringsBits() {
+        $client = $this->getClient();
+        $value = $client->setbit("narwals", 6, 1);
+        $this->assertEquals(0, $value);   
+        $value = $client->setbit("narwals", 6, 0);
+        $this->assertEquals(1, $value);   
+        $value = $client->getbit("narwals", 6);
+        $this->assertEquals(0, $value);   
+    }
+
+    function testHash() {
+        $client = $this->getClient();
+        $ok = $client->hexists("narwals", "bacon");
+        $this->assertEquals(0, $ok);
+        $value = $client->hget("narwals", "bacon");
+        $this->assertEquals(null, $value);   
+        $value = $client->hset("narwals", "bacon", "yummy");
+        $this->assertEquals(1, $value);   
+        $ok = $client->hexists("narwals", "bacon");
+        $this->assertEquals(1, $ok);
+        $value = $client->hget("narwals", "bacon");
+        $this->assertEquals("yummy", $value);   
+        $hash = array(
+            'bacon'   => 'vegan',
+            'narwals' => 'mousse',
+            'penguin' => 'butter',
+        );
+        $value = $client->hmset("narwals", $hash);
+        $this->assertEquals(true, $value);   
+        $keys = array_keys($hash);
+        $keys[] = "zomboez";
+        $value = $client->hmget("narwals", $keys);
+        $values = array_values($hash);
+        $values[] = null;
+        $this->assertEquals($values, $value);
+        $keys = $client->hkeys("narwals");
+        $this->assertEquals(array_keys($hash), $keys);
+        $keys = $client->hvals("narwals");
+        $this->assertEquals(array_values($hash), $keys);
+    }
+
+    function testPipeline() {
+        $client = $this->getClient();
+        $client->pipe();
+        $client->set("aa", "bb");
+        $client->append("aa", "cc");
+        $client->get("aa");
+        $response = $client->unpipe();
+        $expectedResponse = array(
+            true, 4, 'bbcc'
+        );
+        $this->assertEquals($expectedResponse, $response);
+    }
+
+    function testPipelineIsReadMultipleTimes() {
+        $client = new sider_Client(array(
+            'readBuffer' => 0,
+            'writeBuffer' => 0,
+        ));
+        $client->connect();
+        $client->select(3);
+        $client->flushdb();
+        $client->pipe();
+        $f = 1000000;
+        $client->set("aa", str_repeat('X', $f));
+        $i = 3;
+        while ($i--) {
+            $client->append("aa", str_repeat('Y', $f));
+        }
+        $response = $client->unpipe();
+        $expectedResponse = array(
+            true, 2*$f, 3*$f, 4*$f
+        );
+        $this->assertEquals($expectedResponse, $response);
+        $expectedMetrics = array (
+          'socketReads' => 4,
+          'socketWrites' => 6,
+          'commandsSend' => 6,
+          'repliesReceived' => 6,
+        );
+        $this->assertEquals($expectedMetrics, $client->metrics());
+    }
+
+}
